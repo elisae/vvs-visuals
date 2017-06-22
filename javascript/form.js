7028bff4c0c3e1187b0aa6d52e727af91a425a3f
Elisa E <ee014@hdm-stuttgart.de>
2017-06-22 14:46:42 +0200

    Change default to show data from last 4 hours

--- javascript/form.js
+++ javascript/form.js
@@ -1,9 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("startDateField").value = "2017-05-21";
-  document.getElementById("endDateField").value = "2017-05-21";
-  document.getElementById("startHoursField").value = "14:00";
-  document.getElementById("endHoursField").value = "15:00";
+  var now = new Date();
+  // default: show data from the last two hours
+  document.getElementById("endDateField").value = toInputTypeDate(now);
+  document.getElementById("endHoursField").value = now.getHours() + ":" + now.getMinutes();
+  now.setHours(now.getHours() - 4);
+  document.getElementById("startDateField").value = toInputTypeDate(now);
+  document.getElementById("startHoursField").value = now.getHours() + ":" + now.getMinutes();
 });
+
+function toInputTypeDate(dateObject) {
+  var month = dateObject.getMonth() + 1;
+  var string = dateObject.getFullYear() + "-"
+    + (month < 10 ? "0" : "") + month.toString()
+    + "-" + dateObject.getDate();
+  return string;
+}
 
 function toTimestamp(dateString, hourString) {
   var date = new Date(dateString + "T" + hourString + ":00+02:00");
