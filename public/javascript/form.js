8b388e36510ed26874da37bced264a3fc6cb12c3
Elisa E <ee014@hdm-stuttgart.de>
2017-06-22 15:09:40 +0200

    Fix for input type time

--- public/javascript/form.js
+++ public/javascript/form.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
   var now = new Date();
   // default: show data from the last two hours
   document.getElementById("endDateField").value = toInputTypeDate(now);
-  document.getElementById("endHoursField").value = now.getHours() + ":" + now.getMinutes();
+  document.getElementById("endHoursField").value = toInputTypeTime(now);
   now.setHours(now.getHours() - 4);
   document.getElementById("startDateField").value = toInputTypeDate(now);
-  document.getElementById("startHoursField").value = now.getHours() + ":" + now.getMinutes();
+  document.getElementById("startHoursField").value = toInputTypeTime(now);
 });
 
 function toInputTypeDate(dateObject) {
@@ -13,6 +13,14 @@ function toInputTypeDate(dateObject) {
   var string = dateObject.getFullYear() + "-"
     + (month < 10 ? "0" : "") + month.toString()
     + "-" + dateObject.getDate();
+  return string;
+}
+
+function toInputTypeTime(dateObject) {
+  var hours = dateObject.getHours();
+  var minutes = dateObject.getMinutes();
+  var string = (hours < 10 ? "0" : "") + hours.toString()
+    + ":" + (minutes < 10 ? "0" : "") + minutes.toString();
   return string;
 }
 
